33746d963f5aa766caba484b2890c31ac3d475f9
Artur Manukyan <nowrim@mail.ru>
2022-03-17 19:08:45 +0300

    Добавьте файлы проекта.

---
+++ Laba5/Program11/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let f = function
+    | "f#" | "prolog"       -> "Подлиза!"
+    | "java"                -> "Серьёзный дядя"
+    | "r"                   -> "эээёёёёр"
+    | "python"              -> "Я спрашивал про язык программирования"
+    | "c++"| "c#"           -> "Красавчик"
+    | "pascal"| "pascalabc" -> "Поздравляю, вы прошли программирование"
+    | "русский"             -> "Хороший выбор!"
+    | _                     -> "Ну ты и выдал"
+
+[<EntryPoint>]
+let main argv =
+    
+    Console.WriteLine("Ваш любимый язык программирования?")
+    let lang = Console.ReadLine().ToLower()
+    Console.WriteLine(f lang)
+ 
+    0 
