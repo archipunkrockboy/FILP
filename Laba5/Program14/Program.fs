0978f447630b6a1f5ed58fa4a14e461ad0d89861
Artur Manukyan <nowrim@mail.ru>
2022-03-17 22:51:37 +0300

    Выполнена задача 14

---
+++ Laba5/Program14/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let DividersProcessing x f init = 
+    let rec DividersProcessing1 x f init currentDivider = 
+        if currentDivider = 0 then init
+        else 
+            if x % currentDivider = 0 then 
+                let newInit = f init currentDivider 
+                let newDivider = currentDivider - 1
+                DividersProcessing1 x f newInit newDivider
+            else  
+                let newDivider = currentDivider - 1
+                DividersProcessing1 x f init newDivider
+    DividersProcessing1 x f init x
+
+
+
+
+[<EntryPoint>]
+let main argv =
+    
+
+    Console.WriteLine(DividersProcessing 12 (fun x y -> x + y) 0)
+    Console.WriteLine(DividersProcessing 12 (fun x y -> x * y) 1)
+    0 
