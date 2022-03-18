3c662cf233a28c4489574c6ffa2bc4670307da0a
Artur Manukyan <nowrim@mail.ru>
2022-03-18 18:52:32 +0300

    Выполнена задача 18.3

--- Laba5/Program18/Program.fs
+++ Laba5/Program18/Program.fs
@@ -14,11 +14,39 @@ let Sum3 x =
                 Sum3 x1 sum
     Sum3 x 0
 
-
+//подсчёт количества взаимнопростых чисел заданного числа с текущим делителем
+let CountPrimeNumber x currentDivider = 
+    let rec CountPrimeNumber1 x currentDivider count = 
+        if x = 0 then count
+        else 
+            if Program.NOD (x%10) currentDivider = 1 then
+                let x = x/10
+                let count = count + 1
+                CountPrimeNumber1 x currentDivider count
+            else 
+                let x = x/10
+                CountPrimeNumber1 x currentDivider count
+    CountPrimeNumber1 x currentDivider 0
+//делитель, являющийся взаимнопростым с наибольшим количеством цифр заданного числа
+let MaxPrimeNumber x = 
+    let rec MaxPrimeNumber1 x currentDivider maxCount maxDivider = 
+        if currentDivider = 0 then maxDivider
+        else 
+            if CountPrimeNumber x currentDivider > maxCount then
+                let maxCount = CountPrimeNumber x currentDivider    
+                let maxCount1 = maxCount
+                let maxDivider = currentDivider
+                let currentDivider1 = currentDivider - 1
+                MaxPrimeNumber1 x currentDivider maxCount maxDivider
+            else 
+                let currentDivider1 = currentDivider - 1
+                MaxPrimeNumber1 x currentDivider1 maxCount maxDivider
+    MaxPrimeNumber1 x x 0 0
 
 [<EntryPoint>]
 let main argv =
     
     Console.WriteLine(Program.EulerNumber 20)//18.1
     Console.WriteLine(Sum3 133692)//18.2
+    Console.WriteLine(MaxPrimeNumber 28)//18.3
     0 
