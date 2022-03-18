c6950a28aabe5938b103e188487e9daf7df45afb
Artur Manukyan <nowrim@mail.ru>
2022-03-18 21:07:08 +0300

    Выполнена задача 20

---
+++ Laba5/Program20/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let ChoiceFunction = function
+    | 1 -> Program.EulerNumber
+    | 2 -> Program.Sum3
+    | _ -> Program.MaxPrimeNumber
+
+
+[<EntryPoint>]
+let main argv =
+
+    Console.WriteLine("Введите номер функции и число: ")
+    let data = (Console.ReadLine() |> Int32.Parse, Console.ReadLine() |> Int32.Parse)
+    let result = ChoiceFunction (fst data) (snd data)
+    printfn "Ответ: %d"  result
+    0
