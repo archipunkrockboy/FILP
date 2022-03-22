cda55378fd91b78eef2ed716924ba48737ff698d
Artur Manukyan <nowrim@mail.ru>
2022-03-22 23:54:16 +0300

    Выполнена задача 32

---
+++ Laba6/Program32/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let LocalMax list = 
+    let rec LocalMax1 list maxElem count = 
+        match list with
+        []-> count
+        |head :: tail -> if head = maxElem then LocalMax1 tail maxElem  (count+1)
+                         else LocalMax1 tail maxElem count
+    LocalMax1 list (fst(Program.FirstMax list)) 0 
+
+[<EntryPoint>]
+let main argv =
+    
+    let list = Program.ReadData 
+    Console.WriteLine(LocalMax list)
+
+    0 
