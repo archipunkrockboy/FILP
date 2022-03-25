f220b4b5a8596e769e3effa34ddaefe0cc03c755
Artur Manukyan <nowrim@mail.ru>
2022-03-25 19:45:29 +0300

    Решена задача 5

---
+++ Laba7/Program5/Program.fs
@@ -0,0 +1,45 @@
+﻿open System
+
+//сколько x в list
+let Frequency list x = 
+    let rec Frequence list x count = 
+        match list with 
+        []->count
+        |head :: tail -> if head = x then Frequence tail x (count+1)
+                         else Frequence tail x count
+    Frequence list x 0
+
+//самый часто встрочающийся элемент list
+let MaxFrequencyElem list = 
+    let rec MaxFrequencyElem1 list maxElem maxCount = 
+        match list with 
+        []-> maxElem
+        |head :: tail -> if Frequency list head > maxCount then 
+                            MaxFrequencyElem1 tail head (Frequency list head)
+                         else MaxFrequencyElem1 tail maxElem maxCount
+    MaxFrequencyElem1 list list.Head (Frequency list list.Head)
+
+
+//индексы самого часто встречающегося элемента массива
+let MaxFrequencyElemIndexes list = 
+    let rec MaxFrequencyElemIndexes1 list maxElem currentIndex resultList = 
+        match list with
+        [] -> resultList
+        |head :: tail -> if head = maxElem then
+                            MaxFrequencyElemIndexes1 tail maxElem (currentIndex+1)(List.append resultList [currentIndex]) 
+                         else 
+                            MaxFrequencyElemIndexes1 tail maxElem (currentIndex+1) resultList
+    MaxFrequencyElemIndexes1 list (MaxFrequencyElem list) 0 []
+ 
+[<EntryPoint>]
+let main argv = 
+
+
+    let list = Program.ReadData
+    Program.WriteList list
+    Console.WriteLine(Frequency list 5)
+    Console.WriteLine(MaxFrequencyElem list)
+    Program.WriteList(MaxFrequencyElemIndexes list)
+
+
+    0
