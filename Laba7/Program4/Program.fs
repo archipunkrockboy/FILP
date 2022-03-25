32d511827387c47979c91710c234c4bf07f8da9a
Artur Manukyan <nowrim@mail.ru>
2022-03-25 18:54:09 +0300

    Решена задача 4

---
+++ Laba7/Program4/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+//количество элементов на  отрезке А Б 
+let CountElemOnAB list a b = 
+    let rec CountElemOnAB1 list a b resultList= 
+        match list with
+        []-> resultList
+        |head :: tail -> if head >= a && head <= b then 
+                                                    CountElemOnAB1 tail a b (List.append resultList [head])
+                         else CountElemOnAB1 tail a b resultList
+    CountElemOnAB1 list a b []
+
+[<EntryPoint>]
+let main argv = 
+    
+    let list = Program.ReadData
+    Program.WriteList list
+
+    Program.WriteList (CountElemOnAB list 0 10)
+
+    0
