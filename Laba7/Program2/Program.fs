37c272cbb0b6881ea5a022a2c3766afb5fcbdd29
Artur Manukyan <nowrim@mail.ru>
2022-03-25 18:05:21 +0300

    Решена задача 2

--- Laba7/Program2/Program.fs
+++ Laba7/Program2/Program.fs
@@ -1,10 +1,26 @@
 ﻿open System
 
+//элементы до N
+let ElemBeforeMin list = 
+    let rec ElemBeforeMin1 list currentIndex minIndex resultList = 
+        match list with
+        []->resultList
+        |head :: tail -> 
+                         if (currentIndex < minIndex) then 
+                            ElemBeforeMin1 tail (currentIndex+1) minIndex (List.append resultList [head])
+                         else resultList
+                         
+    ElemBeforeMin1 list 0  (List.findIndex (fun x->x = (List.min list)) list) []
+
+
+
+
+
 [<EntryPoint>]
 let main argv =
     
-    let list = ReadData
-    WriteList list 
-    Console.WriteLine(Min2 list)
-    Console.WriteLine(Min2 list)
+    let list = Program.ReadData 
+    Program.WriteList list 
+    let a = ElemBeforeMin list
+    Program.WriteList a
     0
