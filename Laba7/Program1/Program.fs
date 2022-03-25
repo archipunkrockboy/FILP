3e58aa07523e996dc17cc1b6bac880585e8d7ed7
Artur Manukyan <nowrim@mail.ru>
2022-03-25 15:57:34 +0300

    Решена задача 1

--- Laba7/Program1/Program.fs
+++ Laba7/Program1/Program.fs
@@ -1,13 +1,41 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿open System
 
-open System
+let rec ReadList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = ReadList (n-1)
+    Head::Tail
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let ReadData = 
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    ReadList n
 
+let rec WriteList = function
+    [] ->   let z = System.Console.ReadKey()
+            Console.WriteLine()
+            0
+    | (head : int)::tail -> 
+                       System.Console.Write(head)
+                       System.Console.Write(" ")
+                       WriteList tail  
+let Min2 (list: 'a list) =     
+    let minIndex1 = List.findIndex (fun x -> x = List.min list) list
+    let list = List.removeAt minIndex1 list
+    let minIndex2 = List.findIndex (fun x -> x = List.min list) list
+    if minIndex2 >= minIndex1 then (minIndex1, (minIndex2+1))
+    else (minIndex1, minIndex2)
+
+    
+    
+    
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    
+    let list = ReadData
+    WriteList list
+    
+    Console.WriteLine(Min2 list)
+    
+
+    0 
--- Laba7/Program2/Program.fs
+++ Laba7/Program2/Program.fs
@@ -1,13 +1,10 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+﻿open System
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    
+    let list = ReadData
+    WriteList list 
+    Console.WriteLine(Min2 list)
+    Console.WriteLine(Min2 list)
+    0
