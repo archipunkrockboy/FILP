f9b8126d9b40812742e79fa1c783373aeaf7dabd
Artur Manukyan <nowrim@mail.ru>
2022-04-08 19:29:19 +0300

    переделал 4

--- Laba7/Program4/Program.fs
+++ Laba7/Program4/Program.fs
@@ -3,21 +3,24 @@
 //38
 
 //количество элементов на  отрезке А Б 
-let CountElemOnAB list a b = 
-    let rec CountElemOnAB1 list a b resultList= 
-        match list with
-        []-> resultList
-        |head :: tail -> if head >= a && head <= b then 
-                                                    CountElemOnAB1 tail a b (List.append resultList [head])
-                         else CountElemOnAB1 tail a b resultList
-    CountElemOnAB1 list a b []
+//let CountElemOnAB list a b = 
+    //let rec CountElemOnAB1 list a b resultList= 
+        //match list with
+        //[]-> resultList
+        //|head :: tail -> if head >= a && head <= b then 
+                                                    //CountElemOnAB1 tail a b (List.append resultList [head])
+                         //else CountElemOnAB1 tail a b resultList
+    //CountElemOnAB1 list a b []
 
+let CountElemOnAB (list:'a list) a b = 
+    List.fold (fun acc elem -> if elem>=a && elem<=b then acc+1 else acc+0) 0 list
+    
 [<EntryPoint>]
 let main argv = 
     
     let list = Program.ReadData
     Program.WriteList list
-
-    Program.WriteList (CountElemOnAB list 0 10)
+    Console.WriteLine (CountElemOnAB list 0 10)
+    
 
     0
