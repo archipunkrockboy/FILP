b70432fe3ad027c0a1844132ce4c4a5b89d2a9df
Artur Manukyan <nowrim@mail.ru>
2022-04-08 19:23:36 +0300

    переделал 3

--- Laba7/Program3/Program.fs
+++ Laba7/Program3/Program.fs
@@ -3,17 +3,23 @@
 //28
 
 //элементы между первым и последним максимальным
-let ElemBetweenFirstLastMax list= 
-    let rec ElemBetweenFirstLastMax1 list maxIndex resultList = 
-        match list with    
-        [] -> resultList
-        |head :: a :: tail -> if maxIndex <> 1 then 
-                                            ElemBetweenFirstLastMax1 (List.removeAt 1 list) (maxIndex-1) (List.append resultList [a])
+//let ElemBetweenFirstLastMax2 list= 
+    //let rec ElemBetweenFirstLastMax1 list maxIndex resultList = 
+        //match list with    
+        //[] -> resultList
+        //|head :: a :: tail -> if maxIndex <> 1 then 
+                                            //ElemBetweenFirstLastMax1 (List.removeAt 1 list) (maxIndex-1) (List.append resultList [a])
                                             //maxIndex-1 так как из list удалился элемент до него, т.е. он сместился влево
-                              else resultList
-        |head :: tail -> resultList
-    ElemBetweenFirstLastMax1 list (List.findIndexBack (fun x-> x = (List.max list)) list) []
-   
+                              //else resultList
+        //|head :: tail -> resultList
+    //ElemBetweenFirstLastMax1 list (List.findIndexBack (fun x-> x = (List.max list)) list) []
+ 
+ let ElemBetweenFirstLastMax (list: 'a list) = 
+    let maxIndex1 = List.findIndex (fun x->x = List.max list) list
+    let maxIndex2 = List.findIndexBack (fun x->x = List.max (List.removeAt maxIndex1 list)) (List.removeAt maxIndex1 list)
+    if maxIndex1 < maxIndex2 then list[maxIndex1+1 .. maxIndex2] else list[maxIndex2+1 .. maxIndex1-1]
+    
+
 
 
     
