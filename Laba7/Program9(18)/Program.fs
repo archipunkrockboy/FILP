a77a7c6b78dff3efcad89ca91147dad1d94ca62e
Artur Manukyan <nowrim@mail.ru>
2022-04-11 15:18:44 +0300

    выполена задача 18

--- Laba7/Program9(18)/Program.fs
+++ Laba7/Program9(18)/Program.fs
@@ -2,19 +2,30 @@
 
 //18
 
-let SimmetrRazn list1 list2 = 
-    let rec SimmetrRazn1 list1 list2 resultList = 
-        if (list1 = [] && list2 = []) then resultList
-        else 
-            if List.f
+let ReadArray n =
+    let rec read n arr = 
+        match n with 
+        |0 -> arr
+        |_->
+            let newEl=Console.ReadLine()|>Convert.ToInt32
+            let newArr = Array.append arr [|newEl|]
+            read (n-1) newArr
+    read n [||]
 
+let WriteArray arr = 
+    printfn "%A" arr
+
+let SimmetrRazn (arr1: int array) (arr2: int array) = 
+    Array.append (Array.fold (fun acc elem1 -> if (Array.tryFind (fun y -> y = elem1) arr2) <> None then (Array.append acc [||]) else Array.append acc [|elem1|])  [||] arr1) (Array.fold (fun acc elem2 -> if (Array.tryFind (fun y -> y = elem2) arr1) <> None then (Array.append acc [||]) else Array.append acc [|elem2|])  [||] arr2)
+     
+
+
+[<EntryPoint>]
 let main argv = 
-
-
-    let list1 = Program.ReadData
-    let list2 = Program.ReadData
-    Program.WriteList(SimmetrRazn list1 list2)
-
-
+    Console.WriteLine("Первый массив:")
+    let arr1 = Console.ReadLine() |> Convert.ToInt32 |>ReadArray 
+    Console.WriteLine("Второй массив:")
+    let arr2 = Console.ReadLine() |> Convert.ToInt32 |>ReadArray 
+    WriteArray (SimmetrRazn arr1 arr2)
     0
 
