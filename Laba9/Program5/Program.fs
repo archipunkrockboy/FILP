5f7ced26ee941830874a5dc353127e4e7731823f
Artur Manukyan <nowrim@mail.ru>
2022-05-24 21:15:21 +0300

    выполнена задача 5

---
+++ Laba9/Program5/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+open System.Windows.Forms
+open System.Drawing
+open System.IO
+
+let form = new Form(Width=1000, Height = 600, Text = "F# main form", Menu = new MainMenu())//создал форму
+
+let TextBox1= new TextBox(Top= 50,Width =300,Height = 300)//создал текстбокс
+form.Controls.Add(TextBox1)//добавил текстбокс на форму form
+
+let Button1 = new Button(Text="добавить",Top=120,Width=200,Height=30)
+form.Controls.Add(Button1)
+
+
+let ListBox1 = new ListBox(Text="",Top=300,Width=200,Height=300)
+form.Controls.Add(ListBox1)
+
+
+
+let ClickButton click1 = 
+    ListBox1.Items.Insert(0, TextBox1.Text)
+    TextBox1.Clear()
+
+let click1 = Button1.Click.Add(ClickButton)
+Application.Run(form)
